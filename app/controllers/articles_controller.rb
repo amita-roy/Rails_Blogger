7a56cdab79033451b1bcf73e961346e2706b1da1
Ahmad Chata <ahmadchata@gmail.com>
2020-09-24 16:35:22 +0100

    create article helper and refactored create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -12,10 +13,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:article][:title]
-    @article.body = params[:article][:body]
+    @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
-  end
+	end
+	
 end
