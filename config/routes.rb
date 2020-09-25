80136a4aec1f3c047be14e4d74f0cf0147329c7a
Amita <sardana.amita2@gmail.com>
2020-09-25 19:14:23 +0530

    add tag and tagging model, tag controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "articles#index"
   resources :articles do
     resources :comments
   end
-  root to: "articles#index"
+
+  resources :tags
 end
