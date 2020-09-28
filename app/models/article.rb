30521712ee8a69f33c0cfde42e30e80d53a89412
Amita <sardana.amita2@gmail.com>
2020-09-28 19:42:25 +0530

    add form improvment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :taggings
   has_many :tags, through: :taggings, dependent: :destroy
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpeg", "image/png", "image/jpg"]
 
   def tag_list
